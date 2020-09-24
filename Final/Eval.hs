1e8fa4859f1d442a2fb57a790c682cf044f3d7a5
Franco <franco.mellano@gmail.com>
2020-09-23 23:35:54 -0300

    Actualizacion DSL con Grafico Torta

--- Final/Eval.hs
+++ Final/Eval.hs
@@ -1,5 +1,5 @@
 module Eval where
-                 
+
 import qualified Data.Text as T
 
 import GHC.Float(float2Double)              --Importar libreria con funcion float2Double
@@ -22,19 +22,20 @@ thePreamble = do
   usepackage [] tikz
 
 theBody :: [Figure] -> LaTeXT IO ()
-theBody listaFigure = mapM_ (center . tikzpicture . figuretikz) [ figuraFinal( listaFigure )]
+theBody listaFigure = mapM_ (center . tikzpicture . figuretikz) [figuresToFigure(listaFigure)]
 
-figuraFinal :: [Figure] -> Figure
-figuraFinal a = Figures a
+figuresToFigure :: [Figure] -> Figure
+figuresToFigure a = Figures a
 
 formToFigure :: Forma -> Figure
-formToFigure (Cuadrado p x)      = Rectangle (punto2Point p) (float2Double x) (float2Double x)
+formToFigure (Cuadrado p x)       = Rectangle (punto2Point p) (float2Double x) (float2Double x)
 formToFigure (Texto p s)          = Text (punto2Point p) (TeXRaw(T.pack s))
-formToFigure (Rectangulo p x y) = Rectangle (punto2Point p) (float2Double x) (float2Double y)
+formToFigure (Rectangulo p x y)   = Rectangle (punto2Point p) (float2Double x) (float2Double y)
 formToFigure (Poligono a)         = Polygon (map (punto2Point) a)
-formToFigure (Circulo p x)       = Circle (punto2Point p) (float2Double x)
+formToFigure (Circulo p x)        = Circle (punto2Point p) (float2Double x)
 formToFigure (Linea a)            = Line (map (punto2Point) a)
---formToFigure (Elipse p x y)     = Elipse (punto2Point p) (float2Double x) (float2Double y)
+formToFigure (Elipse p x y)       = Ellipse (punto2Point p) (float2Double x) (float2Double y)
+formToFigure (GraficoTorta d)     = figuresToFigure(generarGraficoTorta d 100)
 
 convertForms :: [Forma] -> [Figure]
 convertForms [] = []
@@ -43,3 +44,26 @@ convertForms (x:xs) = [formToFigure x] ++ convertForms xs
 
 punto2Point :: Punto -> Point
 punto2Point (Punto a b) = ((float2Double a), (float2Double b))
+
+--Funciones para grafico de torta
+getFloatFromData :: Dato -> Float
+getFloatFromData (Dato a b) = a
+
+getStringFromData :: Dato -> [Char]
+getStringFromData (Dato a b) = b
+
+generarGraficoTorta :: [Dato] -> Float -> [Figure]
+generarGraficoTorta [] _ = []
+generarGraficoTorta [x] porcentaje = [datoToFigure x porcentaje]
+generarGraficoTorta (x:xs) porcentaje = [datoToFigure x porcentaje] ++ generarGraficoTorta xs (porcentaje - getFloatFromData(x))
+
+datoToFigure :: Dato -> Float -> Figure
+datoToFigure d porcentaje = if porcentaje == 100
+                            then LineWidth (Pt 3) $ Line [(0,0), porcentajeToPuntoLinea(getFloatFromData(d))]
+                            else LineWidth (Pt 3) $ Line [(0,0), porcentajeToPuntoLinea( 100-porcentaje+getFloatFromData(d) )]
+
+porcentajeToPuntoLinea :: Float -> Point
+porcentajeToPuntoLinea p = (float2Double (cos ( (p*360/100) * 2 * pi/360) * 4), float2Double (sin ( (p*360/100) * 2 * pi/360) * 4))
+
+porcentajeToPuntoTexto :: Float -> Point
+porcentajeToPuntoTexto p = (float2Double (cos ( (p*360/100) * 2 * pi/360) * 2.5), float2Double (sin ( (p*360/100) * 2 * pi/360) * 2.5))
