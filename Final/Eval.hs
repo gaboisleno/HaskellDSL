b58adfb53960eaa6cdb20053e9e36b93f3bc6a04
Franco <franco.mellano@gmail.com>
2020-09-24 21:51:13 -0300

    Actualizacion DSL Implementacion Grafico de Torta

--- Final/Eval.hs
+++ Final/Eval.hs
@@ -35,35 +35,43 @@ formToFigure (Poligono a)         = Polygon (map (punto2Point) a)
 formToFigure (Circulo p x)        = Circle (punto2Point p) (float2Double x)
 formToFigure (Linea a)            = Line (map (punto2Point) a)
 formToFigure (Elipse p x y)       = Ellipse (punto2Point p) (float2Double x) (float2Double y)
-formToFigure (GraficoTorta d)     = figuresToFigure(generarGraficoTorta d 100)
+formToFigure (GraficoTorta d)     = figuresToFigure(generarGraficoTorta d)
 
 convertForms :: [Forma] -> [Figure]
 convertForms [] = []
-convertForms [x] = [formToFigure x]
+--convertForms [x] = [formToFigure x]
 convertForms (x:xs) = [formToFigure x] ++ convertForms xs
 
 punto2Point :: Punto -> Point
 punto2Point (Punto a b) = ((float2Double a), (float2Double b))
 
---Funciones para grafico de torta
+{-----Funciones para grafico de torta-----}
+--Obtengo el Float de un Dato
 getFloatFromData :: Dato -> Float
 getFloatFromData (Dato a b) = a
 
+--Obtengo el String de un Dato
 getStringFromData :: Dato -> [Char]
 getStringFromData (Dato a b) = b
-
-generarGraficoTorta :: [Dato] -> Float -> [Figure]
-generarGraficoTorta [] _ = []
-generarGraficoTorta [x] porcentaje = [datoToFigure x porcentaje]
-generarGraficoTorta (x:xs) porcentaje = [datoToFigure x porcentaje] ++ generarGraficoTorta xs (porcentaje - getFloatFromData(x))
-
-datoToFigure :: Dato -> Float -> Figure
-datoToFigure d porcentaje = if porcentaje == 100
-                            then LineWidth (Pt 3) $ Line [(0,0), porcentajeToPuntoLinea(getFloatFromData(d))]
-                            else LineWidth (Pt 3) $ Line [(0,0), porcentajeToPuntoLinea( 100-porcentaje+getFloatFromData(d) )]
 
 porcentajeToPuntoLinea :: Float -> Point
 porcentajeToPuntoLinea p = (float2Double (cos ( (p*360/100) * 2 * pi/360) * 4), float2Double (sin ( (p*360/100) * 2 * pi/360) * 4))
 
 porcentajeToPuntoTexto :: Float -> Point
 porcentajeToPuntoTexto p = (float2Double (cos ( (p*360/100) * 2 * pi/360) * 2.5), float2Double (sin ( (p*360/100) * 2 * pi/360) * 2.5))
+
+--Ejecuto funcion recursiva para generar las lineas del grafico de torta con los datos proporcionados y concateno el circulo base
+generarGraficoTorta :: [Dato] -> [Figure]
+generarGraficoTorta d = generarLineasGraficoTorta d 100 ++ [ LineWidth (Pt 2) $ Circle (0,0) 4, CircleFilled (0,0) 0.05, LineWidth (Pt 3) $ Line [(0,0), (4,0)] ]
+
+--Funcion recursiva para para generar las lineas del grafico de torta junto con el texto de cada Dato
+generarLineasGraficoTorta :: [Dato] -> Float -> [Figure]
+generarLineasGraficoTorta [] _ = []
+--generarLineasGraficoTorta [x] porcentaje = [datoToLineaGraficoTorta x porcentaje]
+generarLineasGraficoTorta (x:xs) porcentaje = [datoToLineaGraficoTorta x porcentaje] ++ generarLineasGraficoTorta xs (porcentaje - getFloatFromData(x))
+
+--Convierto un Dato en una linea y un texto ubicados en la posicion correcta en relacion al porcentaje restante del circulo
+datoToLineaGraficoTorta :: Dato -> Float -> Figure
+datoToLineaGraficoTorta d porcentaje = if porcentaje == 100
+                                        then Figures [LineWidth (Pt 3) $ Line [(0,0), porcentajeToPuntoLinea(getFloatFromData(d))],  Text (porcentajeToPuntoTexto(getFloatFromData(d)/2)) (TeXRaw(T.pack (getStringFromData(d))))]
+                                        else Figures [LineWidth (Pt 3) $ Line [(0,0), porcentajeToPuntoLinea( 100-porcentaje+getFloatFromData(d) )],  Text (porcentajeToPuntoTexto(100-porcentaje+getFloatFromData(d)/2)) (TeXRaw(T.pack (getStringFromData(d))))]
